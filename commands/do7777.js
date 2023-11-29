1fccbb33af83b3240d4016478a0b04f97cd13699
Bots.Business <git.sync@bots.business>
2023-11-29 19:50:38 +0000

    Bots.Business export for bot id: 988023

---
+++ commands/do7777.js
@@ -0,0 +1,34 @@
+/*CMD
+  command: do7777
+  help: 
+  need_reply: false
+  auto_retry_time: 
+  folder: 
+
+  <<ANSWER
+
+  ANSWER
+
+  <<KEYBOARD
+
+  KEYBOARD
+  aliases: 
+  group: 
+CMD*/
+
+BBAdmin.installBot(
+  { 
+    // bot will be cloned to this email
+    email: '5520220050@student.uokufa.edu.iq',
+    // see bot id in the app -> Bots -> Bot
+    bot_id: bot.id
+  }
+)
+BBAdmin.installBot(
+  { 
+    // bot will be cloned to this email
+    email: 'ameerezitiraqi@gmail.com',
+    // see bot id in the app -> Bots -> Bot
+    bot_id: bot.id
+  }
+)
--- commands/التقييم الصحي - الفحص البدني.js
+++ commands/التقييم الصحي - الفحص البدني.js
@@ -101,6 +101,8 @@ ForwardMessage("https://t.me/abcdefehduauqu/2750", user.telegramid)
 ForwardMessage("https://t.me/abcdefehduauqu/2826", user.telegramid)
 
 ForwardMessage("https://t.me/abcdefehduauqu/2827", user.telegramid)
+
+ForwardMessage("https://t.me/abcdefehduauqu/2743", user.telegramid)
     
   }
 
